82d02bab463b45ad928ea010c5252c6ece9f98a6
Njaecha <andre.grabowich@gmail.com>
2025-09-28 18:24:26 +0200

    Bump Version

--- src/AssetImport/AssetImport.cs
+++ src/AssetImport/AssetImport.cs
@@ -22,7 +22,7 @@ namespace AssetImport
     {
         public const string PluginName = "KKS_AssetImport";
         public const string GUID = "org.njaecha.plugins.assetimport";
-        public const string Version = "3.3.0";
+        public const string Version = "3.3.1";
 
         internal new static ManualLogSource Logger;
         internal static AssetSceneController asc;
